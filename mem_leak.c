97dbe920237188c3a5aea656d6ba34e866bd49f3
lyn <liuyanan2017@126.com>
2018-09-05 01:03:06 +0800

    what is memory leak

---
+++ mem_leak.c
@@ -0,0 +1,11 @@
+
+int main(){
+    char  *chunk;
+    while(1){
+     chunk =(char*)(sizeof(1000));
+        printf("Allocating\n");
+
+    }
+}
+
+/*memory leak : only malloc memory but not freed*/
